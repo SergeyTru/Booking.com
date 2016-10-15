aba2bc4f29404268ccb6a285cf48de8b8b7b940c
SergeyTru <seregat@rambler.ru>
2016-10-15 15:36:13 +0300

    Fix test for coordinates

--- server/src/test/java/com/booking/hackathon/CoordinateTest.java
+++ server/src/test/java/com/booking/hackathon/CoordinateTest.java
@@ -2,13 +2,9 @@ package com.booking.hackathon;
 
 import java.util.Arrays;
 import java.util.List;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-/**
- *
- * @author SergeyT
- */
 public class CoordinateTest {
 
   public CoordinateTest() {
@@ -18,7 +14,11 @@ public class CoordinateTest {
   public void testDistance() {
     List<Coordinate> coordinates = Arrays.asList(new Coordinate(40.05, 20.03),
       new Coordinate(40.07, 20.00), new Coordinate(40.09, 19.95));
-    assertEquals(271.15, new Coordinate(40, 20).avgDistanceTo(coordinates), 0.1);
+    assertEquals(8.148, new Coordinate(40, 20).avgDistanceTo(coordinates), 0.1);
   }
 
+  @Test
+  public void testDistanceExact() {
+    assertEquals(2, new Coordinate(17.387111, 40.454988).distanceTo(new Coordinate(17.409176, 40.448332)), 0.1);
+  }
 }
